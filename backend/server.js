fb9fbb4fc2def35a63938fb90faa4c9656717a13
EmbiiFox <camtu.nguyenthi1312200079@gmail.com>
2022-03-31 13:04:16 +0700

    authUser

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,14 @@ import colors from 'colors';
 import connectDB from './config/db.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 dotenv.config();
 
 
 connectDB();
 
 const app=express();
+app.use(express.json());
 
 app.use((req,res,next)=>{
     console.log('HELLO');
@@ -19,32 +21,10 @@ app.use((req,res,next)=>{
 app.get('/',(req,res)=>{
     res.send('API is running')
 })
-app.use('/api/products', productRoutes)
-// app.get('/api/products',(req,res)=>{
-//     res.json(products)
-// })
-// app.get('/api/products/:id',(req,res)=>{
-//     const product=products.find(product=> product._id===req.params.id)
-//     res.json(product)
-// })
-
-//////import vào middlerware
-// app.use((req,res,next)=>{
-//     const error = new Error(`Not Found- ${req.originalUrl}`)
-//     res.status(404);
-//     next(error);
-// })
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
-
-// app.use((err, req, res,next)=>{
-//     const statusCode=res.statusCode === 200?500:res.statusCode
-//     res.status(statusCode);
-//     res.json({
-//         message: err.message,
-//         stack: process.env.NODE_ENV==="production"?null:err.stack
-//     })
-// })
 app.use(errorHandler);
 
 const PORT= process.env.PORT || 5000
